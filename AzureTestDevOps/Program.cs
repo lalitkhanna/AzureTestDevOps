021be5c4ed6aa7ca4bb2ad261d70054a4217788a
lalitkhannaELI <lalitkumar.khanna@eyecareleaders.com>
2021-03-14 14:20:59 +0530

    Update Program.cs
    aaa

--- AzureTestDevOps/Program.cs
+++ AzureTestDevOps/Program.cs
@@ -20,5 +20,6 @@ namespace AzureTestDevOps
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
