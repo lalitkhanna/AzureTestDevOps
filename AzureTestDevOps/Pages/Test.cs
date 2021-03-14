79c3f9140b2ae97415047616ce45d289fc8f649f
lalitkhannaELI <lalitkumar.khanna@eyecareleaders.com>
2021-03-14 14:17:53 +0530

    new test

---
+++ AzureTestDevOps/Pages/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTestDevOps.Pages
+{
+    public class Test
+    {
+        public void myTest()
+        {
+
+        }
+    }
+}
